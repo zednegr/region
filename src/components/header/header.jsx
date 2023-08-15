83f459c769e023688b9e4f05e4d6b46f39448800
zednegr <negrzed@gmail.com>
2023-08-15 12:11:46 +0500

    update

---
+++ src/components/header/header.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect, useRef } from "react"
+import "./header.scss"
+
+
+function Header() {
+
+
+    const [getData, setGetData] = useState([])
+
+
+    let API = 'https://restcountries.com/v3.1/all'
+
+    async function dataFun() {
+        const res = await fetch(API)
+        const getData = await res.json()
+        setGetData(getData)
+    }
+
+    useEffect(() => {
+        dataFun()
+    }, [])
+
+    console.log(getData);
+
+
+
+    return (
+        <>
+            <section>
+                <div className="container">
+                    <div className="card-wrapper">
+
+                        {
+                            getData?.map((item) => (
+                                <div className="card" key={item.id}>
+
+                                    <div className="card-top">
+                                        <img src={item?.flags.svg} alt="flag" width={'400px'} height={'200px'}/>
+                                    </div>
+
+                                    <div className="card-hero">
+                                        <h2>{item?.name.common}</h2>
+                                    </div>
+                                </div>
+                            ))
+                        }
+
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Header
